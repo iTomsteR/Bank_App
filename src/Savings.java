f376f1a6df93e8e6a3fc18320a8d6387890e0299
iTomster <tomsterman97@gmail.com>
2019-09-28 18:20:13 +0300

    Implement safety account features.

--- src/Savings.java
+++ src/Savings.java
@@ -9,13 +9,20 @@ public class Savings extends Account {
     public Savings(String name, String ssn, double initDeposit) {
         super(name, ssn, initDeposit);
         super.modifyAccountNumber("1");
+        setSafetyDepositBox();
     }
 
+    private void setSafetyDepositBox() {
+        safetyDepositBoxId = (int) (Math.random() * Math.pow(10, 3));
+        safetyDepositBoxKey = (int) (Math.random() * Math.pow(10, 4));
+    }
 
     //Common savings acc methods
     @Override
     public String toString() {
-        return super.toString() + "\nAccount Type: Savings";
+        return "\nAccount Type: Savings" + super.toString() +
+                "\nSafety Deposit Box ID: " + safetyDepositBoxId +
+                "\nSafety Deposit Box Key: " + safetyDepositBoxKey;
     }
 
 }
